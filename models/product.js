73887d8118d1dacfd1299ce5c42d3f4d351d0110
Westindiess <gregoryalexiss@gmail.com>
2022-05-06 11:56:56 +0200

    product model

---
+++ models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  title: { type: String, require: true },
+  image: { type: String, require: true },
+  description: { type: String, require: true },
+  price: { type: String, require: true },
+});
+
+module.exports = mongoose.model("Product", productSchema);
