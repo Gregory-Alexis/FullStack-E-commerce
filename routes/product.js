78b2074c32a8d516405c57d96d40f7eca8ff5659
Westindiess <gregoryalexiss@gmail.com>
2022-05-06 11:57:12 +0200

    product route

---
+++ routes/product.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// const auth = require("../middleware/auth");
+
+const productCtrl = require("../controllers/product");
+
+router.get("/", productCtrl.findAllProduct);
+router.get("/:id", productCtrl.findOneProduct);
+
+module.exports = router;
