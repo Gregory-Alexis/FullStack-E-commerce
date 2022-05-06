07dcb16afa1c5e81c008e2e0fb68cba21e096255
Westindiess <gregoryalexiss@gmail.com>
2022-05-07 00:05:35 +0200

    image object data

---
+++ client/src/Main/image.js
@@ -0,0 +1,24 @@
+export const images = [
+  {
+    id: 1,
+    url: "https://res.cloudinary.com/dew6sgune/image/upload/v1651855430/e-commerce/MainImage_dnab13.webp",
+    caption: "Slide 1",
+  },
+  {
+    id: 2,
+    url: "https://res.cloudinary.com/dew6sgune/image/upload/v1651855693/e-commerce/HomeImage2_odg8tn.webp",
+    caption: "Slide 2",
+  },
+  {
+    id: 3,
+    url: "https://res.cloudinary.com/dew6sgune/image/upload/v1651855695/e-commerce/HomeImage3_dxrggb.webp",
+    caption: "Slide 3",
+  },
+];
+
+export const propriete = {
+  duration: 10000,
+  transitionDuration: 500,
+  infinite: true,
+  arrows: false,
+};
