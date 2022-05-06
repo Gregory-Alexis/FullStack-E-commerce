dba7a988716afe9648cae988a480f76c972d71d3
Westindiess <gregoryalexiss@gmail.com>
2022-05-06 17:06:51 +0200

    add Navbar component & font

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./Header/Navbar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ fontFamily: "Montserrat" }}>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
