d20a94e08e86907383dc67f8697311dfaaf0783c
Westindiess <gregoryalexiss@gmail.com>
2022-05-07 00:04:49 +0200

    create transition navbar component on small screen

--- client/src/Header/Navbar.jsx
+++ client/src/Header/Navbar.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import { useRef, useEffect } from "react";
+import { useShop } from "../context/useShop";
+import DotMenu from "../images/vertical-dots.svg";
+import TranslateNavbarOnSmallScreen from "./TranslateNavbarOnSmallScreen";
 
 const Navbar = () => {
+  const { isToggle, dispatch } = useShop();
+  const ref = useRef();
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (isToggle && ref.current && !ref.current.contains(e.target)) {
+        dispatch({ type: "IS_TOGGLE", payload: false });
+      }
+    };
+
+    /*  const changeWidth = () => {
+      dispatch(setWidth(window.innerWidth));
+      if (window.innerWidth >= 1024) {
+        dispatch(setToggle(false));
+      }*/
+
+    //window.addEventListener("resize", changeWidth);
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+      //window.removeEventListener("resize", changeWidth);
+    };
+  }, [dispatch, isToggle]);
+
   return (
-    <header className="bg-blue-700 text-white p-4 flex justify-between items-center">
-      <h1 className="">GregShop</h1>
-      <ul className="flex">
-        <li>Connexion</li>
-        <li>Acceuil</li>
-        <li>Panier</li>
-        <li>Produit</li>
-      </ul>
+    <header ref={ref}>
+      <div className="bg-blue-700 text-white p-4 flex justify-between items-center relative z-20">
+        <h1 className="text-xl">GregShop</h1>
+        <button
+          type="button"
+          aria-label="menu button"
+          className={`transition-all duration-300 ease-in-out ${
+            isToggle ? "-rotate-90" : "rotate-0"
+          }`}
+          onClick={() => dispatch({ type: "IS_TOGGLE", payload: !isToggle })}
+        >
+          <img src={DotMenu} alt="menu" />
+        </button>
+      </div>
+      <TranslateNavbarOnSmallScreen />
     </header>
   );
 };
