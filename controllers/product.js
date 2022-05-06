88a07a260a97a6bd65fedcdf386030a3f4711353
Westindiess <gregoryalexiss@gmail.com>
2022-05-06 16:28:22 +0200

    add createProduct controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,16 @@
 const Product = require("../models/product");
 
+exports.createProduct = async (req, res, next) => {
+  delete req.body.id;
+  const product = new Product({
+    ...req.body,
+  });
+  await product
+    .save()
+    .then(() => res.status(201).json({ message: "Produit enregistré" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
 exports.findAllProduct = (req, res, next) => {
   Product.find()
     .then((product) => res.status(200).json(product))
