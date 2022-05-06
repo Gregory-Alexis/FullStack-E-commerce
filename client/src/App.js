13e28c84e8ec47a59a38535c5c75239f6e626051
Westindiess <gregoryalexiss@gmail.com>
2022-05-07 00:03:38 +0200

    add SliderImage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Header/Navbar";
+import SliderImage from "./Main/SliderImage";
 
 function App() {
   return (
-    <div style={{ fontFamily: "Montserrat" }}>
+    <div style={{ fontFamily: "Montserrat" }} className="min-h-screen">
       <Navbar />
+      <SliderImage />
     </div>
   );
 }
