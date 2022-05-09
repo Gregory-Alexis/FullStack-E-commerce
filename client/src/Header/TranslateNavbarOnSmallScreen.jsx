62eed09e81f0b2a01969164b758351acb68ec172
Westindiess <gregoryalexiss@gmail.com>
2022-05-09 13:01:03 +0200

    change menu color

--- client/src/Header/TranslateNavbarOnSmallScreen.jsx
+++ client/src/Header/TranslateNavbarOnSmallScreen.jsx
@@ -6,22 +6,34 @@ const TranslateNavbarOnSmallScreen = () => {
     <>
       {isToggle ? (
         <div
-          className={`bg-blue-700 text-white w-full absolute left-0 mt-2 transition-all duration-300 ease-in-out translate-y-0 top-12 z-10`}
+          className={`text-primary bg-white w-full absolute left-0 mt-2 opacity-100 transition-all duration-300 ease-in-out translate-y-0 top-12 z-20`}
         >
           <ul className="flex flex-col items-center justify-center h-full">
-            <li className="mb-4 hover:text-blue-300">Connexion</li>
-            <li className="mb-4 hover:text-blue-300">Nos Produits</li>
-            <li className="mb-4 hover:text-blue-300">Mon Panier</li>
+            <li className="my-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Connexion
+            </li>
+            <li className="mb-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Nos Produits
+            </li>
+            <li className="mb-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Mon Panier
+            </li>
           </ul>
         </div>
       ) : (
         <div
-          className={`bg-blue-700 text-white w-full absolute left-0 mt-2 transition-all duration-300 ease-in-out -translate-y-full top-0 z-10`}
+          className={`text-primary bg-white w-full absolute left-0 mt-2 opacity-0 transition-all duration-300 ease-in-out -translate-y-full top-0 z-20`}
         >
           <ul className="flex flex-col items-center justify-center h-full">
-            <li className="mb-4 hover:text-blue-300">Connexion</li>
-            <li className="mb-4 hover:text-blue-300">Nos Produits</li>
-            <li className="mb-4 hover:text-blue-300">Mon Panier</li>
+            <li className="mb-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Connexion
+            </li>
+            <li className="mb-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Nos Produits
+            </li>
+            <li className="mb-4 hover:bg-primary hover:text-white transition-all duration-300 ease-in-out flex justify-center py-2 rounded-lg w-72">
+              Mon Panier
+            </li>
           </ul>
         </div>
       )}
