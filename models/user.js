568c8a17d438b98657ad222a48ef6e4c9232780f
Westindiess <gregoryalexiss@gmail.com>
2022-05-16 12:57:33 +0200

    server

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
-module.exports = mongoose.model("User", userSchema);
