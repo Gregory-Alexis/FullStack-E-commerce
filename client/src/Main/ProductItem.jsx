2aa8561a19ab1426780c223f952eeaf9fa30ff2b
Westindiess <gregoryalexiss@gmail.com>
2022-05-19 23:44:33 +0200

    remove useless code

--- client/src/Main/ProductItem.jsx
+++ client/src/Main/ProductItem.jsx
@@ -1,32 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { addToCart } from "../redux/features/cartSlice";
-import { addToProduct } from "../redux/features/productSlice";
 
 const ProductItem = ({ title, price, image, description, id }) => {
-  const dispatch = useDispatch();
-  const cart = useSelector((state) => state.cartSlice.cart);
-  const cartList = cart.map((element) => {
-    return element.id;
-  });
-
   return (
     <div className="hover:scale-105 transition-all duration-200 ease-in-out">
       <Link to={`/product/${id}`}>
-        <button
-          className="flex flex-col items-center"
-          onClick={() =>
-            dispatch(
-              addToProduct({
-                id: id,
-                title: title,
-                image: image,
-                price: price,
-                description: description,
-              })
-            )
-          }
-        >
+        <button className="flex flex-col items-center">
           <img src={image} alt={title} className="mt-5 h-full " />
         </button>
         <div className="flex justify-between bg-primary text-white p-4 rounded-b-lg">
